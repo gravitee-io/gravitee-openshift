fdadc32805afb82a0fb0db470ead2babc35f6609
Emmanuel <emmanuel.lesne@middleware-solutions.fr>
2017-11-08 17:33:50 +0100

    Deployment into OpenShift 3.6.
    Environment:
    - Windows 10
    - minishift 1.8

---
+++ openshift/start_all.cmd
@@ -0,0 +1,9 @@
+oc login -u developer -p developer
+oc project gravitee
+
+# Start all
+oc scale dc/elasticsearch --replicas=1
+oc scale dc/mongodb --replicas=1
+oc scale dc/gateway --replicas=1
+oc scale dc/managementui --replicas=1
+oc scale dc/managementapi --replicas=1
